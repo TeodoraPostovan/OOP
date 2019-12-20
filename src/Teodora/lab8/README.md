4d3f2350f3a8d058514a67da889c77ea0d87be35
TeodoraPostovan <55695645+TeodoraPostovan@users.noreply.github.com>
2019-12-20 12:37:46 +0200

    Add laboratory 8

---
+++ src/Teodora/lab8/README.md
@@ -0,0 +1,7 @@
+
+
+# LAB8 *Topic: Java Exceptions.*
+---
+## Objectives:
+* Practice with exceptions.
+* Classify different kinds of exceptions.
---
+++ src/Teodora/lab8/UnluckyException.java
@@ -0,0 +1,8 @@
+package Teodora.lab8;
+
+public class UnluckyException extends Exception {
+    public UnluckyException() {
+        super("Ocured an exception!!!");
+    }
+
+}
